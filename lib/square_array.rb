526060a697716e2d8e00cf2006f6122750440fc7
Giulia Cajati <giuliacajati@gmail.com>
2020-05-26 22:36:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
 count = 0 
 new_number =[]
   while count < numbers.length do 
-    new_number.push(numbers.map { |e| e ** 2 }
+    new_number.push(numbers |e| e ** 2 }
     count += 1 
   end
    new_number
