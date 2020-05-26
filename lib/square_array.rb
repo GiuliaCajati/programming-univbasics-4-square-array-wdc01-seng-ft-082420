a12754b7a540ca13a2224948f62195a100ed2734
Giulia Cajati <giuliacajati@gmail.com>
2020-05-26 22:27:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(numbers)
 count = 0 
 new_number =[]
-  while count < array.length do 
-    new_number.push(array[count].sqrt)
+  while count < numbers.length do 
+    new_number.push(numbers[count].sqrt)
     count += 1 
   end
    new_number
